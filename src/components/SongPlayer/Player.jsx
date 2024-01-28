1a282466019ce841634cc3c3941001b3ee63f613
Sanidhya0077 <sanidhyavijay18@gmail.com>
2024-01-28 17:06:26 +0530

    First Commit Updated Volume Button

--- src/components/SongPlayer/Player.jsx
+++ src/components/SongPlayer/Player.jsx
@@ -1,30 +1,32 @@
-import { useRecoilState, useRecoilValue } from "recoil"
-import { seek, volume } from "../../recoil/songPlayerAtoms"
+import { useRecoilState, useRecoilValue } from "recoil";
+import { seek, volume } from "../../recoil/songPlayerAtoms";
 import { useEffect, useRef } from "react";
 import { VolumeBar } from "./VolumeBar";
 
-export function Player({song}){
+export function Player({ song }) {
+  const audioRef = useRef();
+  const [volumeVal, setVolume] = useRecoilState(volume);
+  const seekVal = useRecoilValue(seek);
 
-    const audioRef = useRef();
-    const [volumeVal, setVolume] = useRecoilState(volume);
-    const seekVal = useRecoilValue(seek);
+  useEffect(() => {
+    audioRef.current.volume = volumeVal;
+  }, [volumeVal]);
 
-    useEffect(()=> {
-        audioRef.current.volume = volumeVal;
-    }, [volumeVal])
+  useEffect(() => {
+    audioRef.current.currentTime = seekVal;
+  }, [seekVal]);
 
-    useEffect(()=> {
-        audioRef.current.currentTime = seekVal; 
-    }, [seekVal])
-
-    
-    return(
-        <div className="flex flex-row">
-            <button onClick={()=>{
-                setVolume(0.25);
-            }}>Make Vol 0.25</button>
-            <audio ref={audioRef} controls src={song?.hub?.actions[1]?.uri}></audio>
-            <VolumeBar></VolumeBar>
-        </div>
-    ) 
-} 
+  return (
+    <div className="flex flex-row">
+      <button
+        onClick={() => {
+          setVolume(0.25);
+        }}
+      >
+        Make Volume 0.25
+      </button>
+      <audio ref={audioRef} controls src={song?.hub?.actions[1]?.uri}></audio>
+      <VolumeBar></VolumeBar>
+    </div>
+  );
+}
