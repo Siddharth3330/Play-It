4bc22567d4eb606f5f4052f2fe3f8bbe35a70756
Siddharth <siddharthsingh58000@gmail.com>
2024-02-08 14:02:23 +0530

    Song time added in seekbar

--- src/components/SongPlayer/SeekBar.jsx
+++ src/components/SongPlayer/SeekBar.jsx
@@ -5,14 +5,19 @@ export function SeekBar(){
 
     const songTimeVal= useRecoilValue(songTime)
     const setSeekTime = useSetRecoilState(seekTime)
+    const time= (val)=>{
+        return `${Math.floor(val/60)}:${(`0${Math.floor(val%60)}`).slice(-2)}`;
+    }
 
     return(
         <div>
+            <div>{time(songTimeVal)}</div>
             <button onClick={()=>{setSeekTime(songTimeVal -5)}}>-</button>
             <p></p>
             <input type="range" min="0" step="any" max="90" value={songTimeVal} onInput={(event)=>{setSeekTime(event.target.value)}}></input>
             <p></p>
             <button onClick={()=>{setSeekTime(songTimeVal +5)}}>+</button>
+            <div>1:30</div>
         </div>
     )
 }
